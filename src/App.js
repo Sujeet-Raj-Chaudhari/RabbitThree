95a163252a824bd08848dc916b8292a83ec6772e
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2022-01-22 19:42:55 +0530

    FinalCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from '@react-three/fiber';
+import './style.css';
+import { Physics, usePlane, useBox } from "@react-three/cannon";
+import { OrbitControls, Stars } from "@react-three/drei";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Box(){
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+  return(
+    <mesh onClick={() => {
+      api.velocity.set(0, 5, 0);
+    }}
+    ref={ref}
+    position={[0, 0.5, 0]}
+    >
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color ="hotpink" />
+    </mesh>
   );
 }
 
-export default App;
+function Plane(){
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+
+  return(
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
+
+export default function App(){
+  return(
+    <Canvas>
+      <ambientLight intensity ={0.5} />
+      <spotLight position ={[10, 15, 10]} angle ={0.3} />
+      <OrbitControls />
+      <Stars />
+
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+      
+    </Canvas>
+  );
+};
